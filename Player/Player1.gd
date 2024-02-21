2feba01897d9fe7be1acd5e4b103c38354611b53
Probably-Idiot <zadokfernando@gmail.com>
2024-02-21 12:21:35 +0000

    Changed players

--- Player/Player1.gd
+++ Player/Player1.gd
@@ -1,37 +1,28 @@
 extends RigidBody2D
 
 #Consts
-const SPEED = 4
-const JUMP_VEL = -4
-const GRAVITY = 4
+const SPEED = 175
+@export var JUMP_VEL = 4
+@export var GRAVITY = 6
 
 #Vars
 var is_on_floor = false
-var velocity = Vector2()
+#var velocity = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if $RayCast2D.is_colliding():
+func _physics_process(delta):
+	if $floorCheck.body_entered:
 		is_on_floor = true
+	if $floorCheck.body_exited:
+		is_on_floor = false
 	print(is_on_floor)
 	
-	# Handle Jump
-	#if Input.is_action_pressed("1up") and is_on_floor == true:
-	#	velocity.y = JUMP_VEL
-
-	#Handle movement
-	if is_on_floor == true:
-		var direction = Input.get_axis("1left", "1right")
-		if direction:
-			velocity.x = direction * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-	#else:
-		#velocity.y += GRAVITY
-		
-	#move_and_collide(velocity)
+	if Input.is_action_pressed("1right") and is_on_floor:
+		apply_central_impulse(Vector2(SPEED, 0))
+	if Input.is_action_pressed("1left") and is_on_floor:
+		apply_central_impulse(Vector2(-SPEED, 0))
+	
+	
