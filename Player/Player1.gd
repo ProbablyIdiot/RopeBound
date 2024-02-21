685a21f92e860b9da43e8b25d95122f67af9a393
Probably-Idiot <zadokfernando@gmail.com>
2024-02-21 14:50:47 +0000

    They get stuck together - pls fix

--- Player/Player1.gd
+++ Player/Player1.gd
@@ -2,27 +2,35 @@ extends RigidBody2D
 
 #Consts
 const SPEED = 175
-@export var JUMP_VEL = 4
-@export var GRAVITY = 6
+const JUMP_VEL = -1000
+#const GRAVITY = 6
 
 #Vars
 var is_on_floor = false
-#var velocity = Vector2()
+var can_double_jump = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-func _physics_process(delta):
-	if $floorCheck.body_entered:
-		is_on_floor = true
-	if $floorCheck.body_exited:
-		is_on_floor = false
-	print(is_on_floor)
-	
+func _physics_process(_delta):
+	#print(jump_count)
 	if Input.is_action_pressed("1right") and is_on_floor:
 		apply_central_impulse(Vector2(SPEED, 0))
 	if Input.is_action_pressed("1left") and is_on_floor:
 		apply_central_impulse(Vector2(-SPEED, 0))
 	
-	
+	if Input.is_action_pressed("1up") and is_on_floor == true:
+		apply_central_impulse(Vector2(linear_velocity.y, JUMP_VEL))
+	elif Input.is_action_pressed("1up") and can_double_jump == true:
+		apply_central_impulse(Vector2(linear_velocity.y, JUMP_VEL))
+		can_double_jump == false
+
+
+func _on_floor_check_body_entered(body):
+	can_double_jump
+	is_on_floor = true
+
+
+func _on_floor_check_body_exited(body):
+	is_on_floor = false
--- Player/Player2.gd
+++ Player/Player2.gd
@@ -1,28 +1,38 @@
-extends CharacterBody2D
+extends RigidBody2D
+
+#Consts
+const SPEED = 175
+const JUMP_VEL = -1000
+#const GRAVITY = 6
+
+#Vars
+var is_on_floor = false
+var can_double_jump = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(_delta):
+	#print(jump_count)
+	if Input.is_action_pressed("2right") and is_on_floor:
+		apply_central_impulse(Vector2(SPEED, 0))
+	if Input.is_action_pressed("2left") and is_on_floor:
+		apply_central_impulse(Vector2(-SPEED, 0))
+	
+	if Input.is_action_pressed("2up") and is_on_floor == true:
+		apply_central_impulse(Vector2(linear_velocity.y, JUMP_VEL))
+	elif Input.is_action_pressed("2up") and can_double_jump == true:
+		apply_central_impulse(Vector2(linear_velocity.y, JUMP_VEL))
+		can_double_jump == false
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+func _on_floor_check_body_entered(body):
+	can_double_jump
+	is_on_floor = true
 
 
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+func _on_floor_check_body_exited(body):
+	is_on_floor = false
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("2up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("2left", "2right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
